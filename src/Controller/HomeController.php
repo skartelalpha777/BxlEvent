d51b0f35ae8bdecfb6f9543dc1703ab0927a03a5
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2026-04-06 18:06:30 +0200

    Ajout d'un template, adaptation en cours

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,8 @@ use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/home_events')]
 final class HomeController extends AbstractController
@@ -29,6 +31,27 @@ final class HomeController extends AbstractController
         ]);
     }
 
+/*
+        #[Route('/selected', name: 'home_selected', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+*/
 
 }
