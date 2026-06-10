1583a1c8113b3f71aa71181db6e85d1e74576c92
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2026-06-10 14:02:37 +0200

    ajout du script permettant la reactualisation de la page tickets

---
+++ public/assets/js/ticket.js
@@ -0,0 +1,44 @@
+const FRAIS_SERVICE = 2.50;
+
+function updateQty(idTicket, variation) {
+    const champInput = document.getElementById('qty-' + idTicket);
+    let nouvelleQuantite = parseInt(champInput.value) + variation;
+
+    // On vérifie que la quantité soit pas superieur a 10 et pas inferieur a 0
+    if (nouvelleQuantite >= 0 && nouvelleQuantite <= 10) {
+        champInput.value = nouvelleQuantite;
+        calculerPanier(); // On met à jour le visuel à droite
+    }
+}
+
+// Fonction qui recalcule le résumé de la commande
+function calculerPanier() {
+    let totalTickets = 0;
+    let sousTotal = 0;
+
+    document.querySelectorAll('.ticket-qty').forEach(function (input) {
+        let quantite = parseInt(input.value);
+
+        if (quantite > 0) {
+            totalTickets += quantite;
+
+            let prixUnitaire = parseFloat(input.getAttribute('data-price'));
+            sousTotal += (quantite * prixUnitaire);
+        }
+    });
+
+    // Mise à jour de l'affichage
+    if (totalTickets > 0) {
+        document.getElementById('summary-text').innerText = totalTickets + " ticket(s)";
+        document.getElementById('summary-subtotal').innerText = sousTotal + " €";
+        document.getElementById('summary-total').innerText = (sousTotal + FRAIS_SERVICE) + " €";
+
+
+    } else {
+        document.getElementById('summary-text').innerText = "Aucun ticket sélectionné";
+        document.getElementById('summary-subtotal').innerText = "0 €";
+        document.getElementById('summary-total').innerText = "0 €";
+
+
+    }
+}
