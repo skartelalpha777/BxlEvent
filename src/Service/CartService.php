10baa53865c64560c3c895c235a79ec2914fa774
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2026-07-06 16:43:42 +0200

    function pour ajouter des articles dans le panier realiser

--- src/Service/CartService.php
+++ src/Service/CartService.php
@@ -19,10 +19,25 @@ class CartService
         $this->ticketTypeRepository = $ticketTypeRepository;
     }
 
-   public  function addToCart( int $ticketTypeId, int $quantity): void
+    public  function addToCart(int $ticketTypeId, int $quantity): void
     {
-     
 
+        //recuperation de la session
+        $session = $this->requestStack->getSession();
 
+        // Récupère le panier actuel ou initialise un tableau vide
+        $cart = $session->get('cart', []);
+
+        // Incrémente la quantité si l'article existe déjà, sinon l'ajoute
+        if (!empty($cart[$ticketTypeId])) {
+            $cart[$ticketTypeId] = $quantity;
+        } else {
+            $cart[$ticketTypeId] = $quantity;
+        }
+        if ($quantity <= 0) {
+            unset($cart[$ticketTypeId]);
+        }
+
+        $session->set('cart', $cart);
     }
 }
