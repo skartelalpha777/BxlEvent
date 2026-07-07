c107db8ec19aa096032332fbf4d9453cb5745a94
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2026-07-07 21:19:26 +0200

    fonction pour ajouter des élément dans le panier implemté

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Service\CartService;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/cart')]
 final class CartController extends AbstractController
 {
- 
+
     #[Route(name: 'app_cart_index')]
     public function index(CartService $cartService): Response
     {
@@ -20,6 +22,27 @@ final class CartController extends AbstractController
             'serviceFee' => $cartService->getServicefee(),
             'total' => $cartService->getTotal() + $cartService->getServicefee(),
         ]);
+    }
+
+    #[Route('{id}/add', name: 'app_cart_add', methods: ['POST', 'GET'])]
+    public function add(Event $event, CartService $cartService, Request $request): Response
+    {
+
+        $tickets = $request->request->all('tickets');
+
+        if (empty($tickets)) {
+            $this->addFlash('notice', 'Aucun ticket n\'a été sélectionné.');
+            return $this->redirect($request->headers->get('referer'));
+        }
+
+        foreach ($tickets as $id => $quantity) {
+
+            $cartService->addToCart((int)$id, (int)$quantity);
+        }
+
+        $this->addFlash('success', 'le ou les tickets selectionés ont bien été ajoutés à votre panier.');
+
+        return $this->redirectToRoute('app_event_tickets', ['id' => $event->getId()]);
     }
 
     #[Route('/remove/{id}', name: 'app_cart_remove')]
