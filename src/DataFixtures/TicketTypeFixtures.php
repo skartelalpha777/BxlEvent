59de88f845e41daefd0ccdbf91d13cf95b009eb5
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2026-08-27 12:22:53 +0200

    remplacment de l'enum par un simple string

--- src/DataFixtures/TicketTypeFixtures.php
+++ src/DataFixtures/TicketTypeFixtures.php
@@ -4,7 +4,6 @@ namespace App\DataFixtures;
 
 use App\Entity\Event;
 use App\Entity\TicketType;
-use App\Enum\TicketLabel;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -15,47 +14,47 @@ class TicketTypeFixtures extends Fixture implements DependentFixtureInterface
     {
         $data = [
         'event_0' => [
-            ['label' => TicketLabel::STANDART, 'price' => 55, 'description' => 'Accès général en fosse'],
-            ['label' => TicketLabel::VIP, 'price' => 140, 'description' => 'Accès VIP avec meet & greet'],
-            ['label' => TicketLabel::PROMO, 'price' => 40, 'description' => 'Tarif réduit pour les étudiants'],
+            ['label' => 'Standart', 'price' => 55, 'description' => 'Accès général en fosse'],
+            ['label' => 'Vip', 'price' => 140, 'description' => 'Accès VIP avec meet & greet'],
+            ['label' => 'Promo', 'price' => 40, 'description' => 'Tarif réduit pour les étudiants'],
         ],
         'event_1' => [
-            ['label' => TicketLabel::STANDART, 'price' => 60, 'description' => 'Accès général debout'],
-            ['label' => TicketLabel::VIP, 'price' => 130, 'description' => 'Accès balcon assis'],
+            ['label' => 'Standart', 'price' => 60, 'description' => 'Accès général debout'],
+            ['label' => 'Vip', 'price' => 130, 'description' => 'Accès balcon assis'],
         ],
         'event_2' => [
-            ['label' => TicketLabel::STANDART, 'price' => 75, 'description' => 'Accès général en fosse'],
-            ['label' => TicketLabel::VIP, 'price' => 180, 'description' => 'Carré or, premier rang'],
-            ['label' => TicketLabel::TABLE, 'price' => 220, 'description' => 'Table VIP pour 4 personnes'],
+            ['label' => 'Standart', 'price' => 75, 'description' => 'Accès général en fosse'],
+            ['label' => 'Vip', 'price' => 180, 'description' => 'Carré or, premier rang'],
+            ['label' => 'Table', 'price' => 220, 'description' => 'Table VIP pour 4 personnes'],
         ],
         'event_3' => [
-            ['label' => TicketLabel::STANDART, 'price' => 45, 'description' => 'Accès général debout'],
-            ['label' => TicketLabel::VIP, 'price' => 95, 'description' => 'Accès VIP avec zone dédiée'],
-            ['label' => TicketLabel::PROMO, 'price' => 30, 'description' => 'Tarif réduit -18 ans'],
+            ['label' => 'Standart', 'price' => 45, 'description' => 'Accès général debout'],
+            ['label' => 'Vip', 'price' => 95, 'description' => 'Accès VIP avec zone dédiée'],
+            ['label' => 'Promo', 'price' => 30, 'description' => 'Tarif réduit -18 ans'],
         ],
         'event_4' => [
-            ['label' => TicketLabel::STANDART, 'price' => 25, 'description' => 'Accès général'],
-            ['label' => TicketLabel::TABLE, 'price' => 120, 'description' => 'Table haute pour 2 personnes'],
+            ['label' => 'Standart', 'price' => 25, 'description' => 'Accès général'],
+            ['label' => 'Table', 'price' => 120, 'description' => 'Table haute pour 2 personnes'],
         ],
         'event_5' => [
-            ['label' => TicketLabel::STANDART, 'price' => 65, 'description' => 'Pass 1 jour'],
-            ['label' => TicketLabel::VIP, 'price' => 150, 'description' => 'Pass 2 jours + zone VIP'],
-            ['label' => TicketLabel::ENFANT, 'price' => 20, 'description' => 'Tarif enfant (- de 12 ans)'],
+            ['label' => 'Standart', 'price' => 65, 'description' => 'Pass 1 jour'],
+            ['label' => 'Vip', 'price' => 150, 'description' => 'Pass 2 jours + zone VIP'],
+            ['label' => 'Enfant', 'price' => 20, 'description' => 'Tarif enfant (- de 12 ans)'],
         ],
         'event_6' => [
-            ['label' => TicketLabel::STANDART, 'price' => 35, 'description' => 'Accès général'],
-            ['label' => TicketLabel::PROMO, 'price' => 22, 'description' => 'Tarif réduit étudiant'],
+            ['label' => 'Standart', 'price' => 35, 'description' => 'Accès général'],
+            ['label' => 'Promo', 'price' => 22, 'description' => 'Tarif réduit étudiant'],
         ],
         'event_7' => [
-            ['label' => TicketLabel::STANDART, 'price' => 30, 'description' => 'Entrée simple'],
-            ['label' => TicketLabel::VIP, 'price' => 70, 'description' => 'Accès backstage + vestiaire'],
+            ['label' => 'Standart', 'price' => 30, 'description' => 'Entrée simple'],
+            ['label' => 'Vip', 'price' => 70, 'description' => 'Accès backstage + vestiaire'],
         ],
         'event_8' => [
-            ['label' => TicketLabel::STANDART, 'price' => 0, 'description' => 'Entrée gratuite, réservation obligatoire'],
+            ['label' => 'Standart', 'price' => 0, 'description' => 'Entrée gratuite, réservation obligatoire'],
         ],
         'event_9' => [
-            ['label' => TicketLabel::STANDART, 'price' => 15, 'description' => 'Accès à la journée'],
-            ['label' => TicketLabel::ENFANT, 'price' => 5, 'description' => 'Tarif enfant (- de 12 ans)'],
+            ['label' => 'Standart', 'price' => 15, 'description' => 'Accès à la journée'],
+            ['label' => 'Enfant', 'price' => 5, 'description' => 'Tarif enfant (- de 12 ans)'],
         ],
         ];
 
